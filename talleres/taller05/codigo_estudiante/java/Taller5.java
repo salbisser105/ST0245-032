577e5b57de817ff5bd77d94f1f09199882eef51e
salbisser105 <41337754+salbisser105@users.noreply.github.com>
2018-08-22 23:38:54 -0500

    Add files via upload

--- talleres/taller05/codigo_estudiante/java/Taller5.java
+++ talleres/taller05/codigo_estudiante/java/Taller5.java
@@ -1,55 +1,60 @@
-
-package Taller__5;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller5;
 
 /**
-*La clase Taller5 dos tiene como objetivo dar solución al taller5
-*
-*@autor Mauricio Toro, Camilo Paez
-*@version 1
-*/
+ *
+ * @author Santiago_Albisser
+Juan pablo leal
+ */
 public class Taller5 {
-    
-    
+
     /**
-	* @param array es un arreglo de numeros enteros
-	* @param n la longitud del array anterior 
-	*
-	*en este método se busca hacer la suma de los numeros en un
-	*arreglo de forma recursiva.
-	*
-	* @return la suma
-	*/
-	public static int arrayMax(int[] array, int n) {
-		//...
-	}
-    
-    /**
-	* @param start es un contador, nos sirve para saber cuando debemos parar
-	* @param array es un arreglo de numeros enteros, representan volumen
-	* @param target es la meta, el numero que debe alacanzar la suma 
-	*
-	* en este método se busca hacer la suma de los volumnes posibles
-	* de modo que se acomode de tal forma que se alcance el valor target
-	* pista: la clave esta en el numero de elementos y que no siempre se toman
-	* los elementos, en ocaciones pasan por ejemplo en un conjuntos [5,6,7,8] para un
-	* target 12 se toman el 5 y el 7 pasando por 6 sin cogerlo.
-	*
-	*
-	* @return verdadero si hay una combinación que suponga el valor target, falso de lo contrario
-	*/
-	public static boolean groupSum(int start, int[] nums, int target) {
-        //...
+     * @param args the command line arguments
+     */
+    public static int sumaArr ( int[]arr){
+     int sum=0;
+     for (int i = 0; i < arr.length; i++){
+         sum +=arr[i];
+     }
+     return sum;
     }
-	
-	
-	/**
-	* @param n numero entero, hasta que numero se hace la serie
-	*
-	* el metodo se encarga de devolvernos el valor fibonacci en la enesima posicion
-	* @see <a href="https://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci"> fibonnaci <a/>
-	* @return el valor encontrado en long dada la posible cantidad de bits
-	*/
-	public static long fibonacci(int n) { 
-		//... 
-	}
+    public static int[] insertioNsort (int []arr){
+     int var=0;
+     for (int i=0; i<arr.length;i++){
+         for(int j=i; j<arr.length; j++){
+             if(arr[i]>arr[j]){
+                 var=arr[j];
+                 arr[j]=arr[i];
+                 arr[i]=var;
+                 
+             }
+         }
+     }
+        return arr;
+    }
+    
+    public static void main(String[] args) {
+     for (int i = 10000000; i <= 200000000; i = i + 10000000){
+        int[] a = new int[i];
+        for (int j = 0; j < i; j++ ){
+          a[j] = j;
+        }
+        long start = System.currentTimeMillis();
+          sumaArr(a);
+        long fin = System.currentTimeMillis();       
+        System.out.println(fin-start);
+      }
+    }
+
+    
 }
+
