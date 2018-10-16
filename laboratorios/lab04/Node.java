cd20f5250b47cd883285a764e881778ac9d26585
jplealj <37347503+jplealj@users.noreply.github.com>
2018-10-15 21:35:48 -0500

    Add files via upload

---
+++ laboratorios/lab04/Node.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Node {
+    public Node left;
+    public Node right;
+    public String data;
+    
+    public Node(String d){	 
+        data = d;
+    }
+}
